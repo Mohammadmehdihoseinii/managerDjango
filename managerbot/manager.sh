814a0e44f06baed4aa5ed2d0f5e929b3afc1fc50
mohamadmahdi hosini <94799592+Mohammadmehdihoseinii@users.noreply.github.com>
2025-01-23 17:15:00 +0330

    Add Activate env

--- managerbot/manager.sh
+++ managerbot/manager.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+#
+Venv_chack=$false
 function createdata(){
   function createtext() {
     if [ -f "/$DIR/DB_MDjango/DB_MDjango.py" ] 
@@ -25,44 +27,62 @@ function createdata(){
 }
 
 function managerVenv(){
+  function activateVenv(){
+    if [ $Venv_chack ]; then
+        echo "activate Venv ..."
+        source ./.venv/bin/activate
+        Venv_chack=$true
+    else
+      echo "Venv is activate ..."
+    fi
+  }
+
   function installVenv(){
     echo "install venv ..."
     sudo dnf install python3-virtualenv
+    buldVenv
   }
   function buldVenv(){
-    echo "bulding venv ..."
-    python3 -m venv env
+    if [ -d "/$DIR/env" ] 
+    then
+      echo "env found"
+      activateVenv
+    else
+      echo "bulding venv ..."
+      python3 -m venv env
+      activateVenv
+    fi
+    
   }
-  echo "manager Venv"
-  buldVenv || installVenv 
+  buldVenv || installVenv
 }
 function help(){
   echo "Test help app "
 }
 function Main() {
-  	while true 
-		do
-			echo "--------------- Menu App ---------------"
-			echo "1- manager Venv"
-			echo "h - help"
-			read -p "select in menu: " Selectmenu
-			clear
-			#Checking if variable is empty
-			if test -z "$Selectmenu"; then
-				echo "\$ input is null. input => ($Selectmenu)"
+	while true 
+	do
+		echo "--------------- Menu App ---------------"
+		echo "1- manager Venv"
+		echo "h - help"
+		read -p "select in menu: " Selectmenu
+		clear
+		#Checking if variable is empty
+		if test -z "$Selectmenu"; then
+			echo "\$ input is null. input => ($Selectmenu)"
+		else
+			if [[ $Selectmenu =~ ^[0-9]+$ ]]; then
+				#echo "${Selectmenu} is a number"
+				if [ $Selectmenu == 1 ]; then
+					managerVenv
+          			fi
 			else
-				if [[ $Selectmenu =~ ^[0-9]+$ ]]; then
-					#echo "${Selectmenu} is a number"
-					if [ $Selectmenu == 1 ]; then
-						managerVenv
-          fi
-				else
-					#echo "${NUM} is not a number"
-					if [ "$Selectmenu" = "h" ] || [ "$Selectmenu" = "h" ]; then
-						help
-					fi
+				#echo "${NUM} is not a number"
+				if [ "$Selectmenu" = "h" ] || [ "$Selectmenu" = "h" ]; then
+					help
 				fi
-			fi 
+			fi
+		fi 
 	# We can press Ctrl + C to exit the script
 	done 
 }
